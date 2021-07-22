f0d56fb9f6af000234ce2d54b23607909e383bef
Mehdi Okhovat <Mehdi@DESKTOP-Q9JNDQL>
2021-07-22 12:06:28 +0430

    razor pages added

---
+++ src/NinjaRazorPages/Pages/Privacy.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace NinjaRazorPages.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+    public class RegisterUserService
+    {
+        private readonly INotificationAgent _notificationAgent;
+
+        public RegisterUserService(INotificationAgent notificationAgent)
+        {
+            _notificationAgent = notificationAgent;
+        }
+
+        public void Register(string user)
+        {
+            _notificationAgent.SendEmail(user);
+        }
+    }
+
+
+    public interface INotificationAgent
+    {
+        void SendEmail(string username);
+    }
+
+    public class NotificationAgent : INotificationAgent
+    {
+        public void SendEmail(string username)
+        {
+            // send email
+        }
+    }
+
+}
