274ce3b8ae37931ff55ab08073e2349177710045
Mehdi Okhovat <Mehdi@DESKTOP-Q9JNDQL>
2021-07-30 19:20:54 +0430

    chain of responsibility added

--- src/NinjaConsole/Program.cs
+++ src/NinjaConsole/Program.cs
@@ -6,12 +6,56 @@ namespace NinjaConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var firstChain = new FirstChain();
+            var secondChain = new SecondChain();
+            var thirdChain = new ThirdChain();
+
+            firstChain.SetNext(secondChain);
+            secondChain.SetNext(thirdChain);
+
+            firstChain.Handle("Hello Milad ... ");
+
+            Console.Read();
         }
     }
 
+    public abstract class Chain
+    {
+        protected Chain Next;
+        public abstract void Handle(string input);
 
+        public void SetNext(Chain next)
+        {
+            Next = next;
+        }
+    }
 
+    public class FirstChain : Chain
+    {
+        public override void Handle(string input)
+        {
+            var result = input + "*** Hello from fist chain";
+            Console.Write(result);
+            Next.Handle(result);
+        }
+    }
 
+    public class SecondChain : Chain
+    {
+        public override void Handle(string input)
+        {
+            var result = input + "*** Hello from second chain";
+            Console.Write(result);
+            Next.Handle(result);
+        }
+    }
 
+    public class ThirdChain : Chain
+    {
+        public override void Handle(string input)
+        {
+            var result = input + "*** Hello from third chain";
+            Console.Write(result);
+        }
+    }
 }
